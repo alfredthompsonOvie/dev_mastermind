349928a1f71c0b6a0dbd184f42db0dda02daa599
thompson <alfredthompsonovie@gmail.com>
2024-09-16 12:06:09 +0100

    added suspense to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Suspense } from 'react'
 
 import Navbar from "@/app/_components/layout/Navbar";
 import Footer from "@/app/_components/layout/Footer";
@@ -23,9 +24,11 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${inter.className} bg-background text-text`}>
+				<Suspense>
 				<Navbar isLoggedIn={false} />
 				<>{children}</>
 				<Footer />
+			</Suspense>
 			</body>
 		</html>
 	);
