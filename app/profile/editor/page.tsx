19240755a5542e385c10916a57f9474d3ffac49e
thompson <alfredthompsonovie@gmail.com>
2024-09-16 12:47:35 +0100

    canceled editor component

--- app/profile/editor/page.tsx
+++ app/profile/editor/page.tsx
@@ -1,8 +1,9 @@
-import Editor from "@/app/_components/editor/editor";
+// import Editor from "@/app/_components/editor/editor";
 export default function page() {
 	return (
 		<section>
-			<Editor />
+			{/* <Editor /> */}
+			<h1>I,m work on this part -- deploying this is to vercel is generating some errors</h1>
 		</section>
 	);
 }
